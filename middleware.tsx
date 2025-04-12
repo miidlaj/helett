9f542348d8a641dd245490e9f05fe770772df72c
midlaj <mumidlaj@gmail.com>
2025-04-12 11:01:11 +0530

    removed auth from drivers

--- middleware.tsx
+++ middleware.tsx
@@ -5,9 +5,9 @@ import { NextResponse } from "next/server";
 export function middleware(request: NextRequest) {
   const token = request.cookies.get("token")?.value;
 
-  if (!token && request.nextUrl.pathname.startsWith("/drivers")) {
-    return NextResponse.redirect(new URL("/login", request.url));
-  }
+  // if (!token && request.nextUrl.pathname.startsWith("/drivers")) {
+  //   return NextResponse.redirect(new URL("/login", request.url));
+  // }
 
   if (token && ["/login", "/register"].includes(request.nextUrl.pathname)) {
     return NextResponse.redirect(new URL("/drivers", request.url));
